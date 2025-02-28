2b239827e5464e732eeb743178483a310f9e1b67
tudaolw <daylatu951@gmail.com>
2025-02-28 07:42:48 +0000

    ok

--- src/main/java/com/okd4/Demo/hello.java
+++ src/main/java/com/okd4/Demo/hello.java
@@ -3,7 +3,7 @@ package com.okd4.Demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+@RestController
 public class hello {
     @GetMapping("/hello")
     public String hello_func(){
