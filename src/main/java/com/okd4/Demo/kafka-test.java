b45ade17de83572f7aa87a7070257de5ab8cf471
tudaolw <daylatu951@gmail.com>
2025-02-28 07:54:50 +0000

    cm2

---
+++ src/main/java/com/okd4/Demo/kafka-test.java
@@ -0,0 +1,8 @@
+package com.okd4.Demo;
+
+public class Kafka{
+    private static String kafka_bootstrap_server;
+    private static String kafka_user;
+    private static String kafka_topic;
+    
+}
